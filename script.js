1049f9145af47047f2aae9ae4d32fa1d914e00b6
SusanPasek <susanpasekm@gmail.com>
2021-11-29 16:16:59 -0600

    Final. Should done this sooner

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 // Write your JavaScript code here!
+//const { myFetch } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   let faultyList = document.getElementById("faultyItems");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let coPilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus"); 
+ 
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let coPilotName = document.querySelector("input[name=copilotName]");
+   let fuelAmt = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+     
+   form.addEventListener("submit", function(event) {
+       event.preventDefault();
+       let validPilot = validateInput(pilotName);
+       let validCoPilot = validateInput(coPilotName);
+       let validFuelLevel = validateInput(fuelAmt);
+       let validCargoMass = validateInput(cargoMass);
+       if (validPilot === "" || validCoPilot === "" ||
+           validFuelLevel === "" || validCargoMass === "" ) {
+               alert("All fields are required");
+              // event.preventDefault();
+       };
+       let validArr = [validPilot, validCoPilot, validFuelLevel, validCargoMass];
+      
+       formSubmission(faultyList, validArr, pilotStatus, coPilotStatus, fuelStatus, cargoStatus);
+       event.preventDefault();
+   });
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
    
+  
+   let listedPlanetsResponse = myFetch();
+      
 });
